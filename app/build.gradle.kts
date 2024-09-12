21c64ff2474acf387f09e22466e9d9d0d22eff46
Atharok <atharok@duck.com>
2024-09-12 13:41:52 +0200

    New release (1.3.2). Updated Compose BOM.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.atharok.btremote"
         minSdk = 28
         targetSdk = 34
-        versionCode = 7
-        versionName = "1.3.1"
+        versionCode = 8
+        versionName = "1.3.2"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
